d7613ed8f57f60f7e8be2e908387b42e7e436969
rwrrioe <damir100609@gmail.com>
2025-10-18 17:11:34 +0000

    add tests

--- flattener/flattener_test.go
+++ flattener/flattener_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rwrrioe/Discworld/flattener"
 )
 
-func TestFlattenerDefault(t *testing.T) {
+func TestFlattenerArray(t *testing.T) {
 	jsonContent := `[
 		{"Value": "Running", "Text": "Проводится"},
 		{"Value": "Success", "Text": "Состоялся"},
@@ -30,6 +30,32 @@ func TestFlattenerDefault(t *testing.T) {
 		"Running": "Проводится",
 		"Success": "Состоялся",
 		"Failed":  "Не состоялся",
+	}
+
+	for k, v := range expected {
+		if (*result)[k] != v {
+			t.Errorf("Expected %s -> %s, got %s", k, v, (*result)[k])
+		}
+	}
+}
+
+func TestFlattenerDefault(t *testing.T) {
+	jsonContent := `
+		{"Value": "Running", "Text": "Проводится"}`
+
+	tempfile := filepath.Join(os.TempDir(), "test_data.json")
+	if err := os.WriteFile(tempfile, []byte(jsonContent), 0644); err != nil {
+		t.Fatalf("Failed to write temp JSON file : %v", err)
+	}
+
+	f := flattener.NewFlattener()
+	result, err := f.Flatten(tempfile, "Value", "Text")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := map[string]string{
+		"Running": "Проводится",
 	}
 
 	for k, v := range expected {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,1 +1,67 @@
 package parser_test
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/rwrrioe/Discworld/parser"
+)
+
+func toString(m map[string]interface{}) *map[string]string {
+	strMap := make(map[string]string)
+	for k, v := range m {
+		kStr := fmt.Sprint(k)
+		vStr := fmt.Sprint(v)
+		strMap[kStr] = vStr
+	}
+
+	return &strMap
+}
+
+func TestParser(t *testing.T) {
+	toParse := `[
+  {
+    "Value": "AcceptingApplications",
+    "Text": "Прием заявок",
+    "Items": []
+  },
+  {
+    "Value": "AcceptingAppUserEnter",
+    "Text": "Прием заявок; Регистрация участников в аукционном зале",
+    "Items": []
+  }
+]`
+
+	expected := []map[string]string{
+		{
+			"Value": "AcceptingApplications",
+			"Text":  "Прием заявок",
+			"Items": "[]",
+		},
+
+		{
+			"Value": "AcceptingAppUserEnter",
+			"Text":  "Прием заявок; Регистрация участников в аукционном зале",
+			"Items": "[]",
+		},
+	}
+
+	tempfile := filepath.Join(os.TempDir(), "test_parsedata.json")
+	if err := os.WriteFile(tempfile, []byte(toParse), 0644); err != nil {
+		t.Fatalf("Failed to write temp JSON file: %v", err)
+	}
+
+	p := parser.NewParser()
+	parsed, err := p.Parse(tempfile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for k, v := range expected {
+		if toString((*parsed)[k]) != &v {
+			t.Errorf("Expected %v -> %s, got %s", k, v, (*parsed)[k])
+		}
+	}
+}
